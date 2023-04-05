f967a47144aa48f496485f32c3fe3373552ece8a
nika-chkhaidze <nikachkhaidze08@gmail.com>
2023-04-05 17:11:04 +0400

    js update 1

---
+++ app.js
@@ -0,0 +1,26 @@
+let myComputerManufacturer = "apple";
+// console.log(typeof myComputerManufacturer, myComputerManufacturer);
+
+let myComputerMemory = "16gb";
+// console.log(typeof myComputerMemory, myComputerMemory);
+
+let myComputerOS = "macOS";
+// console.log(typeof myComputerOS, myComputerOS)
+
+const computerBuyingYear = 2021;
+// console.log(typeof computerBuyingYear, computerBuyingYear)
+
+const currentYear = 2023;
+// console.log(typeof currentYear, currentYear)
+
+let sentense1 = "my computer is";
+let sentense2 = "it runs on";
+let sentense3 = "and I've been using it for"
+let sentense4 = "years"
+
+const subtr = currentYear - computerBuyingYear;
+// console.log(currentYear - computerBuyingYear);
+
+const add = sentense1+ myComputerManufacturer+sentense2+myComputerOS+sentense3+subtr+sentense4;
+console.log(add)
+
